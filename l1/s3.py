9854187f7bd3e5ef43cd12bc4ac87b6a2d4d5985
OM <pry@pop-os.localdomain>
2019-10-08 18:33:26 +0000

    added s3 upload

--- l1/s3.py
+++ l1/s3.py
@@ -1,4 +1,22 @@
+import boto3
+import pandas as pd
 
+region = "eu-west-1"
+s3_client = boto3.client('s3', region_name=region)
+location = {'LocationConstraint': region }
+
+bucket = "blossom-data-eng-omar-maj"
+
+# 
+data = pd.read_csv('data/free-7-million-company-dataset.zip', compression='zip')
+
+data = data[data['domain'].isna()]
+data.to_parquet('data/data.parquet')
+data.to_json('data/data.json.gz', orient='records', 
+            lines=True, compression='gzip')
+
+s3_client.upload_file("data/data.json.gz", bucket, "data.json.gz")
+s3_client.upload_file("data/data.parquet", bucket, "data.parquet")
 
 
 
